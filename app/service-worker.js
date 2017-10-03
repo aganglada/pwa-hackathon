dbb8fee0b7796d96524ecab29e48448f01e8dbee
Alejandro <aganglada@gmail.com>
2017-10-03 15:17:36 +0100

    added service workers

--- app/service-worker.js
+++ app/service-worker.js
@@ -18,6 +18,64 @@ limitations under the License.
 
 // TODO SW-3 - cache the application shell
 
+(function() {
+  'use strict';
+  const filesToCache = [
+    '/',
+    'index.html',
+    'scripts/main.min.js',
+    'styles/main.css',
+    'images/products/BarrelChair.jpg',
+    'images/products/C10.jpg',
+    'images/products/Cl2.jpg',
+    'images/products/CP03_blue.jpg',
+    'images/products/CPC_RECYCLED.jpg',
+    'images/products/CPFS.jpg',
+    'images/products/CPO2_red.jpg',
+    'images/products/CPT.jpg',
+    'images/products/CS1.jpg',
+    'images/touch/apple-touch-icon.png',
+    'images/touch/chrome-touch-icon-192x192.png',
+    'images/touch/icon-128x128.png',
+    'images/touch/ms-touch-icon-144x144-precomposed.png',
+    'images/about-hero-image.jpg',
+    'images/delete.svg',
+    'images/footer-background.png',
+    'images/hamburger.svg',
+    'images/header-bg.jpg',
+    'images/logo.png'
+  ];
+
+  var staticCacheName = 'pages-cache-v2';
+
+  self.addEventListener('install', function(event) {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+      caches.open(staticCacheName).then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
+
+  self.addEventListener('activate', function(event) {
+    console.log('Activating new service worker...');
+
+    var cacheWhitelist = [staticCacheName];
+
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+})();
+
 // TODO SW-4 - use the cache-first strategy to fetch and cache resources in the
 // fetch event listener
 
